d70e331e9d408e44dea0e6410220942c4ce3b56a
bianca e nicole <trabalholourdaobinibini@gmail.com>
2025-03-07 20:01:22 -0300

    Update exercicio01.js

---
+++ js/exercicio01.js
@@ -0,0 +1,10 @@
+let numeroSorteado= parseInt(Math.random() * 10)
+console.log(numeroSorteado);
+resultado= numeroSorteado + 1;
+console.log(`${numeroSorteado} + 1 = ${resultado}`);
+resultado= numeroSorteado - 1;
+console.log(`${numeroSorteado} - 1 = ${resultado}`);
+resultado= numeroSorteado * 2;
+console.log(`${numeroSorteado} * 2 = ${resultado}`);
+resultado= numeroSorteado / 2;
+console.log(`${numeroSorteado} / 2 = ${resultado}`);
